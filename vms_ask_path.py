f2c5b9219947db650bda350dc2b0ca4a8e21be4c
wwwdavid34 <wwwdavid34@gmail.com>
2017-08-31 16:49:01 -0600

    removed pinfo

---
+++ vms_ask_path.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python2.7
+
+import os,sys
+
+'''
+This program provides a centralized interface for 
+other scripts to ask for paths or constants.
+'''
+
+
+#base_dir='/eog/scratch1/david/SkyTruth_VMS_2nd'
+psql_ip=
+psql_usr=
+psql_pwd=
+base_dir=os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+tmp_dir=os.path.join(base_dir,'working')
+test_db='dbname=postgres user='+psql_usr+' host='+psql_ip+' password='+psql_pwd
+vms_db=os.path.join(base_dir,'VMS_db/SkyTruth_VMS_annual.db')
+vms_db_psql='dbname=skytruthvms user='+psql_usr+' host='+psql_ip+' password='+psql_pwd
+vms_proc_db=os.path.join(base_dir,'VMS_db/SkyTruth_VMS_process.db')
+vms_proc_db_psql='dbname=skytruthvmsproc user='+psql_usr+' host='+psql_ip+' password='+psql_pwd
+vessel_db=os.path.join(base_dir,'VMS_db/vessel.db')
+vessel_db_psql='dbname=vessel user='+psql_usr+' host='+psql_ip+' password='+psql_pwd
+landing_db=os.path.join(base_dir,'landing_db/landing_site.db')
+fishing_db=os.path.join(base_dir,'fishing_db/fishing_ground.db')
+vbd_db=os.path.join(base_dir,'vbd.db')
+vbd_db_psql='dbname=vbd user='+psql_usr+' host='+psql_ip+' password='+psql_pwd
+dist2coast_db=os.path.join(base_dir,'vvcm_dev/nasa_distfromcoast/dist2coast.db')
+trk_tmp_dir=os.path.join(tmp_dir,'track_tmp')
